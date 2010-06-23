77d6a19b0fb8c4e464f908a486c401b47d561665
Louis Schimchowitsch <schim.louis@yahoo.fr>
2010-06-23 20:35:30 +0200

    new objectives on discovery of a base : explore it all

--- src/Intelligence/ScoutObjectives.cpp
+++ src/Intelligence/ScoutObjectives.cpp
@@ -87,7 +87,8 @@ void ScoutObjectives::onUnitShow(BWAPI::Unit* unit){
 			Broodwar -> printf("eStartLocation is NULL, problem...");
 		Broodwar->printf("Ennemy main base found, waiting for objectives");
 		BWTA::Region* region = eStartLocation->getRegion();
-		//explore_region(region);
+
+		explore_region(region);
 		
 	}
 		//We assume that if the ennemy was not spotted and we find a ResourceDepot, then it is the main base : requires fast scout
@@ -100,7 +101,7 @@ bool ScoutObjectives::got_objectives(){
 }
 
 void ScoutObjectives::explore_region(BWTA::Region* region){
-
+	std::list<Position>::iterator pos;
 	BWTA::Polygon polygon = (*region).getPolygon();
 	std::list<Position> to_see;
 	for(std::vector<Position>::iterator it = polygon.begin(); it != polygon.end(); ++it)
@@ -115,23 +116,27 @@ void ScoutObjectives::explore_region(BWTA::Region* region){
 	double max_dist=0;
 	double test_dist=0;
 	Position current_target;
-
-	while(to_see.size() >0){
-		
+	int size=to_see.size();
+	while(size >0){
+		max_dist=0;
+		current_target=current_pos;
 		//Find the farthest point of current_pos
-		for(std::list<Position>::iterator pos = to_see.begin(); pos != to_see.end(); ++pos){
+		for(pos = to_see.begin(); pos != to_see.end(); ++pos){
 			test_dist=(*pos).getApproxDistance(current_pos);
 			if(test_dist > max_dist){
 				max_dist=test_dist;
 				current_target = (*pos);
 			}
 		}
-		
+		Broodwar->printf("ou ? x %d  y %d", current_target.x(), current_target.y());
+		Broodwar->drawCircle(CoordinateType::Screen,current_target.x(), current_target.y(),200,Colors::Yellow, true);
+	//	void drawCircleMap(int x, int y, int radius, Color color = Color(BWAPI::Colors::Green), bool isSolid = false); 
 		objectives.push_back(current_target);
 		current_pos=current_target;
 		to_see.remove(current_pos);
+		size--;
 	}
-
+	Broodwar->printf("Nombre d'objectifs : %d", objectives.size());
 }
 
 
