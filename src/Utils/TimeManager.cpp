3de53a25719fb8958a4c9fe4a19ca47960b6497b
Gabriel <gabriel.synnaeve@gmail.com>
2010-07-02 16:04:08 +0200

    dummy timer test

--- src/Utils/TimeManager.cpp
+++ src/Utils/TimeManager.cpp
@@ -10,9 +10,31 @@ TimeManager::~TimeManager()
 
 }
 
+VOID CALLBACK test_callback( 
+    HWND hwnd,        // handle to window for timer messages 
+    UINT message,     // WM_TIMER message 
+    UINT idTimer,     // timer identifier 
+    DWORD dwTime)     // current system time 
+{
+    Broodwar->printf("timer: %d, %d", message, idTimer);
+}
+
+
 void TimeManager::update()
 {
-	frameCount++;
+    frameCount++;
+    // UINT_PTR WINAPI SetTimer(
+    // __in_opt  HWND hWnd,
+    // __in      UINT_PTR nIDEvent,
+    // __in      UINT uElapse,
+    // __in_opt  TIMERPROC lpTimerFunc
+    // );
+    SetTimer(
+        NULL,   // handle to the window
+        6669,   // reset the timer with the same ID
+        40,     // 40 ms = 25 frames / sec
+        test_callback //suspendAll // callback function when timer expires
+        );
 }
 
 std::string TimeManager::getName() const
@@ -28,4 +50,8 @@ void TimeManager::display() const
 unsigned long TimeManager::getElapsedTime() const
 {
 	return frameCount;
+}
+
+void TimeManager::suspendAll()
+{
 }
--- src/Utils/TimeManager.h
+++ src/Utils/TimeManager.h
@@ -1,11 +1,24 @@
 #pragma once
+#include <windows.h>
 #include <CSingleton.h>
 #include <BWAPI.h>
 #include <string>
 
 using namespace BWAPI;
 
-/*
+
+/** Special exception to abort a thread gently
+*/
+/*class ThreadAbort
+{
+	__declspec (noreturn) static void Throw();
+public:
+	static bool RaiseInThread(HANDLE hThread);
+	static void DontOptimize() throw (...);
+};*/
+
+
+/**
 * The goal of this class is to provide tools related to TimeManager.
 *
 * The frame count since the begining can be retrieved.
@@ -31,6 +44,7 @@ public:
 
 	// TimeManager
 	unsigned long getElapsedTime() const;
+    void suspendAll();
 
 	// TimeManagerr facilities
   //void call();
