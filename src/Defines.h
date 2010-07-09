395eca58adbe5aabb2065825cf01492d6ddb3411
Nicolas <nicolas.biagianti@gmail.com>
2010-07-09 17:39:53 +0200

    merge2

--- src/Defines.h
+++ src/Defines.h
@@ -13,9 +13,5 @@ class MainWindow;
 
 extern QApplication* qapplication;
 extern MainWindow* qmainwindow;
-<<<<<<< HEAD
-#endif 
-=======
 extern bool g_onStartDone;
 #endif
->>>>>>> fb7e1948762ae3334742a0bf1157a942e8515d6e
