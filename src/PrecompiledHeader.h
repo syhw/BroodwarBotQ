d38d0dbcbc2151af4da9c96b31bbcdb93b13fe26
syhw <gabriel.synnaeve@gmail.com>
2011-05-09 09:54:02 +0200

    precompiled header

---
+++ src/PrecompiledHeader.h
@@ -0,0 +1,19 @@
+#ifndef NO_PRECOMPILED_HEADERS
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <assert.h>
+#include <algorithm>
+#include <utility>
+#include <time.h>
+#include <BWTA.h>
+#include <BWAPI.h>
+#include <float.h>
+#include <math.h>
+#endif
