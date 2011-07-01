68949abbbb69d9b7be05fe7d4373903d8275e9f7
syhw <gabriel.synnaeve@gmail.com>
2011-07-01 19:11:04 +0200

    bootstraping the opening prediction

--- src/Intelligence/ETechEstimator.cpp
+++ src/Intelligence/ETechEstimator.cpp
@@ -6,6 +6,7 @@ using namespace BWAPI;
 using namespace std;
 
 ETechEstimator::ETechEstimator()
+: notFirstOverlord(false)
 {
 	{
 		std::ifstream ifs("C:\\StarCraft\\AI\\BroodwarBotQ\\data\\tables\\PvP.table");
@@ -26,22 +27,137 @@ void ETechEstimator::onUnitShow(Unit* u)
 {
 	if (u->getPlayer()->isEnemy(Broodwar->self()))
 	{
+		int recomputeTime = 0;
 		if (u->getType().isBuilding() 
 			|| u->getType() == UnitTypes::Zerg_Overlord)
 		{
-			insertBuilding(u);
+			if (insertBuilding(u))
+				recomputeTime = Broodwar->getFrameCount() / 24;
+		} else {
+			/// We infer the buildings needed to produce this unit
+			for (map<UnitType, int>::const_iterator it = u->getType().requiredUnits().begin();
+				it != u->getType().requiredUnits().end(); ++it)
+			{
+				if (insertBuilding(it->first))
+				{
+					int tmpTime = (Broodwar->getFrameCount()
+						- (it->first.buildTime()
+						// + walktime
+						)) / 24;
+					if (!recomputeTime || tmpTime < recomputeTime) // not quite correct
+						recomputeTime = tmpTime;
+				}
+			}
 		}
+		if (recomputeTime)
+			computeDistribOpenings(recomputeTime);
 	}
-
 }
 
 void ETechEstimator::onUnitHide(Unit* u)
 {
 }
 
-void ETechEstimator::insertBuilding(Unit* u)
+bool ETechEstimator::insertBuilding(UnitType ut)
+{
+	size_t previous_size = buildingsTypesSeen.size();
+	if (ut.getRace() == BWAPI::Races::Protoss)
+	{
+		if (ut == UnitTypes::Protoss_Robotics_Facility)
+			buildingsTypesSeen.insert(Protoss_Robotics_Facility);
+		else if (ut == UnitTypes::Protoss_Observatory)
+			buildingsTypesSeen.insert(Protoss_Observatory);
+		else if (ut == UnitTypes::Protoss_Gateway)
+			buildingsTypesSeen.insert(Protoss_Gateway);
+		else if (ut == UnitTypes::Protoss_Citadel_of_Adun)
+			buildingsTypesSeen.insert(Protoss_Citadel_of_Adun);
+		else if (ut == UnitTypes::Protoss_Cybernetics_Core)
+			buildingsTypesSeen.insert(Protoss_Cybernetics_Core);
+		else if (ut == UnitTypes::Protoss_Templar_Archives)
+			buildingsTypesSeen.insert(Protoss_Templar_Archives);
+		else if (ut == UnitTypes::Protoss_Forge)
+			buildingsTypesSeen.insert(Protoss_Forge);
+		else if (ut == UnitTypes::Protoss_Stargate)
+			buildingsTypesSeen.insert(Protoss_Stargate);
+		else if (ut == UnitTypes::Protoss_Fleet_Beacon)
+			buildingsTypesSeen.insert(Protoss_Fleet_Beacon);
+		else if (ut == UnitTypes::Protoss_Arbiter_Tribunal)
+			buildingsTypesSeen.insert(Protoss_Arbiter_Tribunal);
+		else if (ut == UnitTypes::Protoss_Robotics_Support_Bay)
+			buildingsTypesSeen.insert(Protoss_Robotics_Support_Bay);
+	}
+	else if (ut.getRace() == BWAPI::Races::Terran)
+	{
+		if (ut == UnitTypes::Terran_Comsat_Station)
+			buildingsTypesSeen.insert(Terran_ComSat);
+		else if (ut == UnitTypes::Terran_Nuclear_Silo)
+			buildingsTypesSeen.insert(Terran_Nuclear_Silo);
+		else if (ut == UnitTypes::Terran_Barracks)
+			buildingsTypesSeen.insert(Terran_Barracks);
+		else if (ut == UnitTypes::Terran_Academy)
+			buildingsTypesSeen.insert(Terran_Academy);
+		else if (ut == UnitTypes::Terran_Factory)
+			buildingsTypesSeen.insert(Terran_Factory);
+		else if (ut == UnitTypes::Terran_Starport)
+			buildingsTypesSeen.insert(Terran_Starport);
+		else if (ut == UnitTypes::Terran_Control_Tower)
+			buildingsTypesSeen.insert(Terran_Control_Tower);
+		else if (ut == UnitTypes::Terran_Science_Facility)
+			buildingsTypesSeen.insert(Terran_Science_Facility);
+		else if (ut == UnitTypes::Terran_Covert_Ops)
+			buildingsTypesSeen.insert(Terran_Covert_Ops);
+		else if (ut == UnitTypes::Terran_Physics_Lab)
+			buildingsTypesSeen.insert(Terran_Physics_Lab);
+		else if (ut == UnitTypes::Terran_Machine_Shop)
+			buildingsTypesSeen.insert(Terran_Machine_Shop);
+		else if (ut == UnitTypes::Terran_Engineering_Bay)
+			buildingsTypesSeen.insert(Terran_Engineering_Bay);
+		else if (ut == UnitTypes::Terran_Armory)
+			buildingsTypesSeen.insert(Terran_Armory);
+		else if (ut == UnitTypes::Terran_Missile_Turret)
+			buildingsTypesSeen.insert(Terran_Missile_Turret);
+	}
+	else if (ut.getRace() == BWAPI::Races::Zerg)
+	{
+		if (ut == UnitTypes::Zerg_Lair)
+			buildingsTypesSeen.insert(Zerg_Lair);
+		else if (ut == UnitTypes::Zerg_Hive)
+			buildingsTypesSeen.insert(Zerg_Hive);
+		else if (ut == UnitTypes::Zerg_Nydus_Canal)
+			buildingsTypesSeen.insert(Zerg_Nydus_Canal);
+		else if (ut == UnitTypes::Zerg_Hydralisk_Den)
+			buildingsTypesSeen.insert(Zerg_Hydralisk_Den);
+		else if (ut == UnitTypes::Zerg_Defiler_Mound)
+			buildingsTypesSeen.insert(Zerg_Defiler_Mound);
+		else if (ut == UnitTypes::Zerg_Greater_Spire)
+			buildingsTypesSeen.insert(Zerg_Greater_Spire);
+		else if (ut == UnitTypes::Zerg_Queens_Nest)
+			buildingsTypesSeen.insert(Zerg_Queens_Nest);
+		else if (ut == UnitTypes::Zerg_Evolution_Chamber)
+			buildingsTypesSeen.insert(Zerg_Evolution_Chamber);
+		else if (ut == UnitTypes::Zerg_Ultralisk_Cavern)
+			buildingsTypesSeen.insert(Zerg_Ultralisk_Cavern);
+		else if (ut == UnitTypes::Zerg_Spire)
+			buildingsTypesSeen.insert(Zerg_Spire);
+		else if (ut == UnitTypes::Zerg_Spawning_Pool)
+			buildingsTypesSeen.insert(Zerg_Spawning_Pool);
+		else if (ut == UnitTypes::Zerg_Creep_Colony)
+			buildingsTypesSeen.insert(Zerg_Creep_Colony);
+	}
+	else
+	{
+		BWAPI::Broodwar->printf("ERROR: ETechEstimator failed to determine the race of %s", ut.getName().c_str());
+	}
+	if (buildingsTypesSeen.size() > previous_size)
+		return true;
+	else
+		return false;
+}
+
+bool ETechEstimator::insertBuilding(Unit* u)
 {
 	UnitType ut = u->getType();
+	size_t previous_size = buildingsTypesSeen.size();
 	if (ut.getRace() == BWAPI::Races::Protoss)
 	{
 		if (ut == UnitTypes::Protoss_Nexus)
@@ -62,8 +178,6 @@ void ETechEstimator::insertBuilding(Unit* u)
 			if (tmp <= Protoss_Pylon3)
 				buildingsTypesSeen.insert(tmp);
 		}
-		else if (ut == UnitTypes::Protoss_Robotics_Facility)
-			buildingsTypesSeen.insert(Protoss_Robotics_Facility);
 		else if (ut == UnitTypes::Protoss_Assimilator)
 		{
 			int tmp = Protoss_Assimilator;
@@ -214,11 +328,16 @@ void ETechEstimator::insertBuilding(Unit* u)
 		}
 		else if (ut == UnitTypes::Zerg_Overlord)
 		{
-			int tmp = Zerg_Overlord;
-			while (buildingsSeen.count(u))
-				++tmp;
-			if (tmp <= Zerg_Overlord3)
-				buildingsTypesSeen.insert(tmp);
+			if (notFirstOverlord)
+			{
+				int tmp = Zerg_Overlord;
+				while (buildingsSeen.count(u))
+					++tmp;
+				if (tmp <= Zerg_Overlord3)
+					buildingsTypesSeen.insert(tmp);
+			}
+			else
+				notFirstOverlord = true;
 		}
 	}
 	else
@@ -226,4 +345,12 @@ void ETechEstimator::insertBuilding(Unit* u)
 		BWAPI::Broodwar->printf("ERROR: ETechEstimator failed to determine the race of %s", ut.getName().c_str());
 	}
 	buildingsSeen.insert(u);
+	if (buildingsTypesSeen.size() > previous_size)
+		return true;
+	else
+		return false;
+}
+
+void ETechEstimator::computeDistribOpenings(int time)
+{
 }
--- src/Intelligence/ETechEstimator.h
+++ src/Intelligence/ETechEstimator.h
@@ -11,9 +11,12 @@ class ETechEstimator : public CSingleton<ETechEstimator>
 	serialized_tables st;
 	std::set<BWAPI::Unit*> buildingsSeen; 
 	std::set<int> buildingsTypesSeen;
+	bool notFirstOverlord;
+	bool insertBuilding(BWAPI::Unit* u);
+	bool insertBuilding(BWAPI::UnitType ut);
+    void computeDistribOpenings(int time);
 public:
 	void onUnitDestroy(BWAPI::Unit* u);
 	void onUnitShow(BWAPI::Unit* u);
 	void onUnitHide(BWAPI::Unit* u);
-	void insertBuilding(BWAPI::Unit* u);
 };
