8167eeccc94b4a6f4a7627f3a4b1a900491be311
syhw <gabriel.synnaeve@gmail.com>
2011-07-31 16:13:38 +0200

    compiling KillSubgoal

--- src/Micro/Goals/KillSubgoal.cpp
+++ src/Micro/Goals/KillSubgoal.cpp
@@ -1,26 +1,29 @@
-#include "KillSubgoal.h"
+#include <PrecompiledHeader.h>
+#include "Micro/Goals/KillSubgoal.h"
+#include "Macro/InformationManager.h"
 
-KillSubgoal::KillSubgoal(SubgoalLogic l, Unit* u) : 
-Subgoal(l),
-target(u)
+KillSubgoal::KillSubgoal(SubgoalLogic l, UnitsGroup* ug, Unit* u) 
+: Subgoal(l, ug)
+, _target(u)
 {
-
-}
-bool KillSubgoal::isRealized(){
-	return this->check();
-
 }
 
-bool KillSubgoal::check(){
-return false;
+bool KillSubgoal::isRealized()
+{
+	return _target != NULL && TheInformationManager->exists(_target);
 }
 
-double KillSubgoal::distanceToRealize(UnitsGroup *ug){
-return 0;
-
+double KillSubgoal::distanceToRealize()
+{
+	return -1;
 }
 
-void KillSubgoal::tryToRealize(UnitsGroup * ug){
-
-
+void KillSubgoal::tryToRealize()
+{
+	/// If we call tryToRealize on a "-1" distanceToRealize Subgoal
+	/// it should be the last and only Subgoal
+	if (_target->isVisible())
+		_unitsGroup->attack(_target->getPosition());
+	else
+		_unitsGroup->attack(TheInformationManager->getLastPosition(_target));
 }
